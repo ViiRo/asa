8ebcfb2d31b0370e985a7fbb6f9854137256f0dd
ViiRo <45905915+ViiRo@users.noreply.github.com>
2019-01-04 06:03:20 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("523827935011143680")
+let channel =     client.channels.get("530741175489921044")
 setInterval(function() {
 channel.send(`**Credit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit CreditCredit Credit**`);
 }, 30)
